f3033f020cef424f9a5717f6ec0487bdd781c0df
Brittney Epperson <beppers87@hotmail.com>
2024-07-31 21:28:39 +0000

    Added timestamp endpoint method

--- index.js
+++ index.js
@@ -24,7 +24,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:dateInput?", (req, res) => {
+  const dateInput = req.params.dateInput;
+  let date;
 
+  if (!dateInput) {
+    date = new Date();
+  } else {
+    if (!isNaN(dateInput)) {
+      date = new Date(parseInt(dateInput));
+    }
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({error: date.toString() });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
